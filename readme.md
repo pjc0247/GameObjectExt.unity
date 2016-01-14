3e5fdc53393dd57c3fe0534b619ad3a2bc59e5ec
pjc02478 <pjc0247@gmail.com>
2016-01-14 15:55:21 +0900

    init

---
+++ readme.md
@@ -0,0 +1,59 @@
+GameObjectExt
+====
+
+__Find__
+```c#
+var obj = GameObject.Find("object");
+
+var obj = "object".Find();
+```
+
+__Find<T>__
+```c#
+var component = GameObject.Find("object").GetComponent<Foo>();
+
+var component = "object".Find<Foo>();
+```
+
+__Transform__
+```
+var t = GameObject.Find("object").transform;
+
+var t = "object".Transform();
+```
+
+__SetParent__
+```
+var child = GameObject.Find("child").transform;
+var parent = GameObject.Find("parent").transform;
+child.SetParent(parent);
+
+"child".SetParent("parent");
+// or
+"child".SetParent(parentGameObject);
+"child".SetParent(parentTransform);
+```
+
+Shorcuts for UI Components
+----
+__GetText__
+```c#
+var msg = GameObject.Find("object").GetComponent<Text>().text;
+
+var msg = "object".GetText();
+```
+
+__GetSelectedOption__
+```c#
+var dropdown = GameObject.Find("object").GetComponent<Dropdown>();
+var msg = dropdown.options[dropdown.value];
+
+var msg = "object".GetSelectedOption();
+```
+
+__IsOn__
+```c#
+var isOn = GameObject.Find("object").GetComponent<Toggle>().isOn;
+
+var isOn = "object".IsOn();
+```
---
+++ src.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+    public static class GameObjectUtil
+    {
+        public static Transform Transform(
+            this string name)
+        {
+            return GameObject.Find(name).transform;
+        }
+
+        public static GameObject Find(
+            this string name)
+        {
+            return GameObject.Find(name);
+        }
+        public static T Find<T>(
+            this string name)
+            where T : UnityEngine.MonoBehaviour
+        {
+            return GameObject.Find(name).GetComponent<T>();
+        }
+        public static T Find<T>(
+            this Type type)
+            where T : UnityEngine.Object
+        {
+            return GameObject.FindObjectOfType<T>();
+        }
+
+        public static void SetActive(
+            this string name,
+            bool value)
+        {
+            name.Find().SetActive(value);
+        }
+        public static void SetParent(
+            this string child,
+            string parent)
+        {
+            child.Transform().SetParent(parent.Transform());
+        }
+        public static void SetParent(
+            this string child,
+            GameObject parent)
+        {
+            child.Transform().SetParent(parent.transform);
+        }
+        public static void SetParent(
+            this string child,
+            Transform parent)
+        {
+            child.Transform().SetParent(parent);
+        }
+
+        public static string GetText(
+            this string name)
+        {
+            return name.Find<Text>().text;
+        }
+        public static void SetText(
+            this string name, string value)
+        {
+            name.Find<Text>().text = value;
+        }
+
+        public static bool IsOn(
+            this string name)
+        {
+            return name.Find<Toggle>().isOn;
+        }
+        public static string GetSelectedOption(
+            this string name)
+        {
+            var dropdown = name.Find<Dropdown>();
+            return dropdown.options[dropdown.value].text;
+        }
+        public static int GetSelectedIndex(
+            this string name)
+        {
+            var dropdown = name.Find<Dropdown>();
+            return dropdown.value;
+        }
+    }
