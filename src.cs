8c68a913debb7f2d512a6529a260269dd5fc1d65
Park June Chul <pjc0247@naver.com>
2016-01-27 13:46:52 +0900

    Update src.cs

--- src.cs
+++ src.cs
@@ -23,7 +23,7 @@ using UnityEngine.UI;
         }
         public static T Find<T>(
             this string name)
-            where T : UnityEngine.MonoBehaviour
+            where T : UnityEngine.Component
         {
             return GameObject.Find(name).GetComponent<T>();
         }
